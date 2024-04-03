d80972f72fdb0583316d192f983cb18932e2532a
Blake Duncan <blakecduncan@gmail.com>
2024-04-02 21:38:40 -0400

    update waas package

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import createAccount from './createAccount'
 import { KernelAccountClient } from '@zerodev/sdk'
 import { useWalletConnect } from '@zerodev/waas'
 import { polygonMumbai, polygon, base } from "viem/chains"
+import { zeroAddress } from 'viem'
 import './App.css'
 
 const baseZeroDevProjectId = import.meta.env.VITE_BASE_PROJECT_ID ?? ''
@@ -17,6 +18,7 @@ function App() {
   const [account, setAccount] = useState()
   const [uri, setUri] = useState('')
   const [chain, setChain] = useState<'base' | 'polygon' | 'mumbai'>('base')
+  const [isDeployed, setIsDeployed] = useState(false)
 
   useEffect(() => {
     const getAccount = async () => {
@@ -44,11 +46,22 @@ function App() {
     getAccount()
   }, [chain])
 
+  useEffect(() => {
+    const isContractWallet = async () => {
+      if (!account) return false;
+      const bytecode = await (account as KernelAccountClient).account.client.getBytecode({
+        address: (account as KernelAccountClient).account.address
+      });
+      setIsDeployed(!!bytecode);
+    }
+    isContractWallet()
+  }, [account, chain])
+
   const {
     connect,
     sessionProposal,
-    approve,
-    reject, 
+    approveSessionProposal,
+    rejectSessionProposal, 
     isLoading, 
     error, 
     disconnect, 
@@ -60,6 +73,17 @@ function App() {
     kernelClient: account
   })
 
+  const deployWallet = async () => {
+    if (!account) return;
+    console.log('deploying wallet')
+    const txnHash = await (account as KernelAccountClient).sendTransaction({
+      to: zeroAddress,
+      value: BigInt(0),
+      data: "0x",
+    })
+    console.log("txn hash:", txnHash)
+  }
+
   if (!account) {
     return <p className="text-center text-lg">Loading...</p>
   }
@@ -68,32 +92,44 @@ function App() {
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-2xl font-bold text-center mb-4">ZeroDev WalletConnect Demo</h1>
       {account && (
-        <>
+        <div>
           <p className="text-center mb-2"><strong>Address: </strong> {(account as KernelAccountClient).account.address}</p>
           <p className="text-center mb-2"><strong>Chain: </strong> {(account as KernelAccountClient).chain.id}</p>
-        </>
+          <p className="text-center mb-2"><strong>Is Deployed: </strong> {isDeployed ? 'Yes' : 'No'}</p>
+          {!isDeployed && (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+              onClick={deployWallet}
+            >
+              Deploy Wallet
+            </button>
+          )}
+        </div>
       )}
-      <button
-        className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mb-4 ${chain === 'base' && 'opacity-50 cursor-not-allowed'}`}
-        disabled={chain === 'base'}
-        onClick={() => setChain('base')}
-      >
-        Base
-      </button>
-      <button
-        className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mb-4 ${chain === 'polygon' && 'opacity-50 cursor-not-allowed'}`}
-        disabled={chain === 'polygon'}
-        onClick={() => setChain('polygon')}
-      >
-        Polygon
-      </button>
-      <button
-        className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mb-4 ${chain === 'mumbai' && 'opacity-50 cursor-not-allowed'}`}
-        disabled={chain === 'mumbai'}
-        onClick={() => setChain('mumbai')}
-      >
-        Mumbai
-      </button>
+      <div>
+        <h2 className="text-xl font-semibold mb-2">Change Chain</h2>
+        <button
+          className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mb-4 ${chain === 'base' && 'opacity-50 cursor-not-allowed'}`}
+          disabled={chain === 'base'}
+          onClick={() => setChain('base')}
+        >
+          Base
+        </button>
+        <button
+          className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mb-4 ${chain === 'polygon' && 'opacity-50 cursor-not-allowed'}`}
+          disabled={chain === 'polygon'}
+          onClick={() => setChain('polygon')}
+        >
+          Polygon
+        </button>
+        <button
+          className={`bg-blue-500 text-white font-bold py-2 px-4 rounded mb-4 ${chain === 'mumbai' && 'opacity-50 cursor-not-allowed'}`}
+          disabled={chain === 'mumbai'}
+          onClick={() => setChain('mumbai')}
+        >
+          Mumbai
+        </button>
+      </div>
       <div className="flex gap-2 mb-4">
         <input
           className="flex-1 p-2 border border-gray-300"
@@ -114,13 +150,13 @@ function App() {
           <div className="flex gap-2 justify-center">
             <button
               className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => approve(sessionProposal)}
+              onClick={() => approveSessionProposal(sessionProposal)}
             >
               Approve
             </button>
             <button
               className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => reject()}
+              onClick={() => rejectSessionProposal()}
             >
               Reject
             </button>
